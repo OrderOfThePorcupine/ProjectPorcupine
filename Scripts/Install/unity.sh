e65911ffcbc7f1a886f69842a31de794a29f69d0
Koosemose <koosemose@users.noreply.github.com>
2017-04-15 02:07:51 -0400

    Update Unity CI install link

--- Scripts/Install/unity.sh
+++ Scripts/Install/unity.sh
@@ -4,7 +4,7 @@
 
 # This link should be changed if the unity version of this project is updated
 echo 'Downloading Unity-5.4.0f3: '
-curl -o Unity.pkg http://download.unity3d.com/download_unity/a6d8d714de6f/MacEditorInstaller/Unity-5.5.3f1.pkg
+curl -o Unity.pkg http://download.unity3d.com/download_unity/4d2f809fd6f3/MacEditorInstaller/Unity-5.5.3f1.pkg
 
 echo 'travis_fold:start:install-unity'
 echo 'Installing Unity.pkg'
