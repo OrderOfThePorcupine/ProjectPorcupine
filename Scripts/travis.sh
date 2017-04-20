897ce4c4302bcbc90d5eff18cb48d6edd5e1872d
Holger Arndt <github@kjarrigan.de>
2017-04-20 11:30:42 +0200

    Add automated, regular binary builds (#51)
    * Build packages for the Windows, Linux, OSX through TravisCI
    
    * [Travis] Replace test-parameter with 'official' ProjectPorcupine values

--- Scripts/travis.sh
+++ Scripts/travis.sh
@@ -3,15 +3,21 @@
 # the environment variable travis selects.
 
 if [ "$JOB" == "unit-test" ]; then
-   
-   ./Scripts/Install/unity.sh
-   ./Scripts/test.sh --travis
-   exit $?
+    ./Scripts/Install/unity.sh
+    ./Scripts/test.sh --travis
+
+    # Only build binary stuff through the cron-job
+    if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+        ./Scripts/build.sh
+    fi
+    exit $?
 fi
 
-
 if [ "$JOB" == "stylecop" ]; then
-	./Scripts/Install/mono.sh
-	./Scripts/Install/stylecop.sh
+    if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+      exit 0
+    fi
+    ./Scripts/Install/mono.sh
+    ./Scripts/Install/stylecop.sh
     ./Scripts/check-style.sh
 fi
