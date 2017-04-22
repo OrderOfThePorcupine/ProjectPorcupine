2f3a8814f14770e247848047c1de827e89c18d98
Koosemose <koosemose@users.noreply.github.com>
2017-04-22 03:38:57 -0400

    Satisfying Stylecop comment related spacing rules.

--- Assets/Scripts/Controllers/Events/TradeController.cs
+++ Assets/Scripts/Controllers/Events/TradeController.cs
@@ -59,6 +59,7 @@ public class TradeController
         SpriteRenderer spriteRenderer = go.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = SpriteManager.GetSprite("Trader", prototype.AnimationIdle.CurrentFrameName);
         spriteRenderer.sortingLayerName = "TradeShip";
+        
         // TODO: Look into passing more of the work of calling a tradeship off to the controller
         controller.Init(leavingCoords: new Vector3(100, 50, 0), landingCoords: new Vector3(landingPad.Tile.X + 1, landingPad.Tile.Y + 1, 0), speed: 5f, trader: trader, animationIdle: prototype.AnimationIdle.Clone(), animationFlying: prototype.AnimationFlying.Clone(), renderer: spriteRenderer);
     }
