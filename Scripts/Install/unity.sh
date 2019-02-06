9f8db4a528a3a6621d10813068dab671bdfdad16
Ben Pearson <tuvas21@gmail.com>
2019-02-06 12:54:01 -0500

    Adding support for unit tests by default, improving ability to change version for unit testing.

--- Scripts/Install/unity.sh
+++ Scripts/Install/unity.sh
@@ -1,43 +1,39 @@
 #! /bin/sh
 
-# See https://unity3d.com/get-unity/download/archive
-# to get download URLs
-UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
-UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/588dc79c95ed/MacEditorInstaller/Unity-2017.2.5f1.pkg"
-UNITY_WINDOWS_TARGET_PACKAGE_URL="https://download.unity3d.com/download_unity/588dc79c95ed/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.2.5f1.pkg"
-
-
-# Downloads a file if it does not exist
-download() {
-
-	URL=$1
-	FILE=`basename "$URL"`
-	
-	# Downloads a package if it does not already exist in cache
-	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
-		echo "$FILE does not exist. Downloading from $URL: "
-		mkdir -p "$UNITY_DOWNLOAD_CACHE"
-		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
-	else
-		echo "$FILE Exists. Path not included."
-		exit 1
-	fi
-}
-
-# Downloads and installs a package from an internet URL
-install() {
-	PACKAGE_URL=$1
-	download $1
-
-	echo "Installing `basename "$PACKAGE_URL"`"
-	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
-}
-
-
-
 echo "Contents of Unity Download Cache:"
 ls $UNITY_DOWNLOAD_CACHE
 
 echo "Installing Unity..."
 install $UNITY_OSX_PACKAGE_URL
 install $UNITY_WINDOWS_TARGET_PACKAGE_URL
+
+# See https://unity3d.com/get-unity/download/archive
+# to get download version names and hashes
+
+BASE_URL=https://netstorage.unity3d.com/unity
+HASH=588dc79c95ed
+VERSION=2017.2.5f1
+
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
