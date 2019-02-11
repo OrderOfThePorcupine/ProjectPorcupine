20779fa8b5bf3bd5f4cb49bff39ada02ad81360c
kd7uiy <tuvas21@gmail.com>
2019-02-11 12:08:33 -0500

    Updates Unit Tests to test JSON code. (#158)
    * Updates Headline Generator Test to test JSON code.
    
    * Changes Schedule Editor to JSON from XML test
    
    * Bribing the style cop.

--- Assets/Editor/UnitTests/Controllers/HeadlineGeneratorTest.cs
+++ Assets/Editor/UnitTests/Controllers/HeadlineGeneratorTest.cs
@@ -6,27 +6,29 @@
 // file LICENSE, which is part of this source code package, for details.
 // ====================================================
 #endregion
-using System.Xml;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
-using UnityEngine.TestTools;
 
 public class HeadlineGeneratorTest
 {
     private HeadlineGenerator gen;
     private bool stringPrinted;
 
-    private string testHeadlineXml = @"
-<Headlines>
-    <Headline>The CEO of Quillcorp, has announced that it main and only shareholder is still the main and only shareholder, Quill18.</Headline>
-    <Headline>Notice: Quillcorp has placed an embargo on ""Chairs"", any Quillcorp Basic Utility Station found in possession of this illegal contraband will be fined 2 million Quillbucks.</Headline>
-</Headlines>";
+    private string testHeadlineJson = @"{
+  ""Headline"": [
+    ""The CEO of Quillcorp, has announced that it main and only shareholder is still the main and only shareholder, Quill18."",
+    ""Notice: Quillcorp has placed an embargo on \""Chairs\"", any Quillcorp Basic Utility Station found in possession of this illegal contraband will be fined 2 million Quillbucks."",
+  ]
+}";
 
     [SetUp]
     public void SetUp()
     {
         PrototypeManager.Initialize();
 
-        PrototypeManager.Headline.LoadPrototypes(testHeadlineXml);
+        JToken reader = JToken.Parse(testHeadlineJson);
+
+        PrototypeManager.Headline.LoadJsonPrototypes((JProperty)reader.First);
 
         gen = new HeadlineGenerator();
         gen.UpdatedHeadline += StringPrinted;
