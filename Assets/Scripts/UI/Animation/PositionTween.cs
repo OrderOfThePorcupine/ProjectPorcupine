ae217aa803d92ac0c775828c070d778ca3c7b398
Onur Tanrıkulu <onurtnrkl@gmail.com>
2019-02-14 14:09:29 +0100

    Slide animation system for UI.
    - SlideAnimation component added for animating menu objects.

---
+++ Assets/Scripts/UI/Animation/PositionTween.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class PositionTween
+{
+    private RectTransform rect;
+    private Vector2 target;
+    private float duration;
+    private Action onEnded;
+    private float elapsedTime;
+
+    public PositionTween(RectTransform rect, Vector2 target, float duration)
+    {
+        this.rect = rect;
+        this.target = target;
+        this.duration = duration;
+    }
+
+    public PositionTween(RectTransform rect, Vector2 target, float duration, Action onEnded) : this(rect, target, duration)
+    {
+        this.onEnded = onEnded;
+    }
+
+    public void Start()
+    {
+        elapsedTime = 0;
+        TimeManager.Instance.EveryFrame += Update;
+    }
+
+    public void Stop()
+    {
+        TimeManager.Instance.EveryFrame -= Update;
+    }
+
+    private void Update(float time)
+    {
+        if (elapsedTime < duration)
+        {
+            elapsedTime += time;
+            float percentage = Mathf.Clamp01(elapsedTime / duration);
+            rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, target, percentage);
+        }
+        else
+        {
+            Stop();
+            if (onEnded != null) onEnded();
+            rect.anchoredPosition = target;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Animation/SlideAnimation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace ProjectPorcupine.UI.Animation
+{
+    public class SlideAnimation : MonoBehaviour
+    {
+        [SerializeField] private float duration;
+        private PositionTween slideIn;
+        private PositionTween slideOut;
+        private Vector2 outPosition;
+        private Vector2 inPosition;
+
+        private void Awake()
+        {
+            RectTransform rect = GetComponent<RectTransform>();
+            //Calculates only left to right position.
+            //TODO: Add All Slide modes(LeftToRight, RightToLeft etc).
+            outPosition = new Vector2(rect.anchoredPosition.x - rect.sizeDelta.x, rect.anchoredPosition.y);
+            inPosition = rect.anchoredPosition;
+            rect.anchoredPosition = outPosition;
+
+            slideIn = new PositionTween(rect, inPosition, duration);
+            slideOut = new PositionTween(rect, outPosition, duration, () => gameObject.SetActive(false));
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            slideOut.Stop();
+            slideIn.Start();
+        }
+
+        public void Hide()
+        {
+            slideIn.Stop();
+            slideOut.Start();
+        }
+    }
+}
