59644e89a7051823a496b2957978f26600b4a2a4
Braedon <Braedonww@gmail.com>
2019-02-17 18:36:40 +1100

    Travis fixes (#189)
    * Just seeing if this fixes things
    * May revert

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,37 +4,76 @@ PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
 UNITY_BUILD_DIR=$(pwd)/Build
 LOG_FILE=$UNITY_BUILD_DIR/unity-win.log
 
-
 ERROR_CODE=0
 echo "Items in project path ($PROJECT_PATH):"
 ls "$PROJECT_PATH"
 
-
-echo "Building project for Windows..."
-mkdir $UNITY_BUILD_DIR
+echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile \
-  -projectPath "$PROJECT_PATH" \
-  -buildWindows64Player  "$(pwd)/build/win/ci-build.exe" \
-  -quit \
-  | tee "$LOG_FILE"
-  
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -logFile $(pwd)/unity.log \
+    -projectPath $(pwd) \
+    -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+    -quit
+
 if [ $? = 0 ] ; then
   echo "Building Windows exe completed successfully."
-  ERROR_CODE=0
 else
   echo "Building Windows exe failed. Exited with $?."
-  ERROR_CODE=1
+  echo $LOG_FILE
+  exit $?
 fi
 
-#echo 'Build logs:'
-#cat $LOG_FILE
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -logFile $(pwd)/unity.log \
+    -projectPath $(pwd) \
+    -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+    -quit
 
-echo "Finishing with code $ERROR_CODE"
-exit $ERROR_CODE
+if [ $? = 0 ] ; then
+  echo "Building Mac exe completed successfully."
+else
+  echo "Building Mac exe failed. Exited with $?."
+  echo $LOG_FILE
+  exit $?
+fi
+
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -logFile $(pwd)/unity.log \
+    -projectPath $(pwd) \
+    -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+    -quit
+
+
+if [ $? = 0 ] ; then
+  echo "Building Linux exe completed successfully."
+else
+  echo "Building Linux exe failed. Exited with $?."
+  echo $LOG_FILE
+  exit $?
+fi
+
+echo 'Attempting to zip builds for week $BUILD_VERSION'
+cd $(pwd)/Build/
+echo 'Attempting to zip linux'
+zip -q -r Linux-$BUILD_VERSION.zip linux/
+echo 'Attempting to zip osx'
+zip -q -r MacOSX-$BUILD_VERSION.zip osx/
+echo 'Attempting to zip windows'
+zip -q -r Windows-$BUILD_VERSION.zip windows/
+cd -
+
+echo "All builds done creating config file"
 
 # Create the config file for Bintray
 generate-bintray-json > ./Scripts/bintray.json
