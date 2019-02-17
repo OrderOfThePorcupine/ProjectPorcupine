64e8ea2cef0570174ef35b0f4e51acd468ddd6b0
Braedon <Braedonww@gmail.com>
2019-02-17 20:04:51 +1100

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -76,4 +76,4 @@ cd -
 echo "All builds done creating config file"
 
 # Create the config file for Bintray
-./generate-bintray-json.sh > ./Scripts/bintray.json
+./Scripts/generate-bintray-json.sh > ./Scripts/bintray.json
