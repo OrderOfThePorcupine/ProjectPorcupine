d785aff98b3c2760ae4bf9bde55fa05851d23373
Braedon <Braedonww@gmail.com>
2019-02-19 12:34:21 +1100

    Fix cursor properly (#203)
    * Revert "Fixes mouse cursor texture issue (#183)"
    
    This reverts commit 57b69dc4fff12b19c9c964fd1e5c18473f2980a1.
    
    * Made the cursor an actual cursor rather than sprite

--- Assets/Scripts/UI/Mouse/MouseCursor.cs
+++ Assets/Scripts/UI/Mouse/MouseCursor.cs
@@ -65,7 +65,7 @@ namespace ProjectPorcupine.Mouse
                 this.style = new GUIStyle() { font = Resources.Load<Font>("Fonts/Arial/Arial") as Font, fontSize = 15, alignment = TextAnchor.MiddleCenter };
             }
 
-            cursorTexture = Resources.Load<Texture2D>("UI/Cursors/Ship.png");
+            cursorTexture = Resources.Load<Texture2D>("UI/Cursors/Ship");
 
             BuildCursor();
         }
