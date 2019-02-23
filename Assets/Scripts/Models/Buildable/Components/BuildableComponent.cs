2bb7c51ea88c4af012629d30d00fa2a0d96ca902
Ben Pearson <tuvas21@gmail.com>
2019-02-23 15:44:15 -0500

    Conditions are now being properly read, including for things like animations. Seems to be working much better!

--- Assets/Scripts/Models/Buildable/Components/BuildableComponent.cs
+++ Assets/Scripts/Models/Buildable/Components/BuildableComponent.cs
@@ -335,6 +335,7 @@ namespace ProjectPorcupine.Buildable.Components
 
             public string ValueBasedParameterName { get; set; }
 
+            [JsonConverter(typeof(ConditionsJsonConvertor))]
             public Conditions RunConditions { get; set; }
         }
 
@@ -390,10 +391,65 @@ namespace ProjectPorcupine.Buildable.Components
             public bool CanUseVariableEfficiency { get; set; }
         }
 
+        public class ConditionsJsonConvertor : JsonConverter
+        {
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType == typeof(Conditions);
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                Conditions condition = new Conditions();
+                condition.ParamConditions = new List<ParameterCondition>();
+                JToken jtoken = JToken.ReadFrom(reader);
+                foreach (JProperty prop in jtoken)
+                {
+                    ConditionType type = (ConditionType)Enum.Parse(typeof(ConditionType), prop.Value.ToString(), true);
+                    condition.ParamConditions.Add(new ParameterCondition()
+                    {
+                        Condition = type,
+                        ParameterName = prop.Name
+                    });
+                }
+
+                return condition;
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                Conditions conditions = (Conditions)value;
+                JObject jObjs = new JObject();
+                foreach (ParameterCondition pCondition in conditions.ParamConditions)
+                {
+                    JProperty prop = new JProperty(pCondition.ParameterName, pCondition.Condition.ToString());
+                    jObjs.Add(prop);
+                }
+                jObjs.WriteTo(writer);
+            }
+        }
+
         [Serializable]
-        [JsonObject(MemberSerialization.OptOut)]
+        [JsonObject(MemberSerialization.OptIn)]
         public class Conditions
         {
+            public static Conditions Deserialize(JToken jtoken)
+            {
+                Conditions condition = new Conditions();
+                condition.ParamConditions = new List<ParameterCondition>();
+                foreach (JProperty prop in jtoken)
+                {
+                    ConditionType type = (ConditionType)Enum.Parse(typeof(ConditionType), prop.Value.ToString(), true);
+                    condition.ParamConditions.Add(new ParameterCondition()
+                    {
+                        Condition = type,
+                        ParameterName = prop.Name
+                    });
+                }
+
+                return condition;
+            }
+
             public List<ParameterCondition> ParamConditions { get; set; }
         }
     }
