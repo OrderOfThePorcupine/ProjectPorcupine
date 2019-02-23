64534a75f426137d90357ab8ce703c683561b5f2
Ben Pearson <tuvas21@gmail.com>
2019-02-23 15:56:44 -0500

    Conditioning with style!

--- Assets/Scripts/Models/Buildable/Components/BuildableComponent.cs
+++ Assets/Scripts/Models/Buildable/Components/BuildableComponent.cs
@@ -419,13 +419,14 @@ namespace ProjectPorcupine.Buildable.Components
             public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
             {
                 Conditions conditions = (Conditions)value;
-                JObject jObjs = new JObject();
-                foreach (ParameterCondition pCondition in conditions.ParamConditions)
+                JObject obj = new JObject();
+                foreach (ParameterCondition param in conditions.ParamConditions)
                 {
-                    JProperty prop = new JProperty(pCondition.ParameterName, pCondition.Condition.ToString());
-                    jObjs.Add(prop);
+                    JProperty prop = new JProperty(param.ParameterName, param.Condition.ToString());
+                    obj.Add(prop);
                 }
-                jObjs.WriteTo(writer);
+
+                obj.WriteTo(writer);
             }
         }
 
@@ -433,23 +434,6 @@ namespace ProjectPorcupine.Buildable.Components
         [JsonObject(MemberSerialization.OptIn)]
         public class Conditions
         {
-            public static Conditions Deserialize(JToken jtoken)
-            {
-                Conditions condition = new Conditions();
-                condition.ParamConditions = new List<ParameterCondition>();
-                foreach (JProperty prop in jtoken)
-                {
-                    ConditionType type = (ConditionType)Enum.Parse(typeof(ConditionType), prop.Value.ToString(), true);
-                    condition.ParamConditions.Add(new ParameterCondition()
-                    {
-                        Condition = type,
-                        ParameterName = prop.Name
-                    });
-                }
-
-                return condition;
-            }
-
             public List<ParameterCondition> ParamConditions { get; set; }
         }
     }
