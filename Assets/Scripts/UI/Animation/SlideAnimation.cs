caaeb5cdb0f49af7762a2ee9d4992ecc4dc26284
Onur Tanrıkulu <onurtnrkl@gmail.com>
2019-02-25 19:06:03 +0300

    Update SlideAnimation.cs
    * Exception changed with InvalidOperationException.

--- Assets/Scripts/UI/Animation/SlideAnimation.cs
+++ Assets/Scripts/UI/Animation/SlideAnimation.cs
@@ -62,7 +62,7 @@ namespace ProjectPorcupine.UI.Animation
                 case SlideDirection.Up:
                     return new Vector2(x, y + height);
                 default:
-                    throw new System.Exception(string.Format("{0} not found.", direction));
+                    throw new System.InvalidOperationException(string.Format("{0} not found.", direction));
             }
         }
     }
